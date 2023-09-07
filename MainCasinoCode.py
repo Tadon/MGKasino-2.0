056aa2e19f0da3f2f9f47655677b2c45ea565a73
Tadon <Mark.Klocek@GMail.com>
2023-09-07 02:36:30 -0700

    new methods in UI class

--- MainCasinoCode.py
+++ MainCasinoCode.py
@@ -1,12 +1,16 @@
+#pulling user functions from user_information.py
 from user_information import UserInformation
 
+#creating filename variable to use read_csv file function to create hash of current users, passwords, and balances(user data)
 filename = 'casino_data.csv'
 user_data = UserInformation.read_csv_file(filename)
 
-#Main casino program
-while True:
-     new_username = input("Enter new user name or type exit to exit: ")
+#Making user login/create user profile before allowing access to casino
+verified_user = False
+while verified_user == False:
+     new_username = input("Enter new user name or type exit to exit: ").lower()
      if new_username.lower() == 'exit':
+          verified_user = True
           break
      
      if new_username in user_data:
@@ -17,5 +21,12 @@ while True:
           UserInformation.add_user_to_database(filename, new_username, new_password)
           print(f'User added! Welcome to MGKasino 2.0, {new_username}!')
           user_data[new_username] = {'password': new_password, 'balance': 10000}
+          verified_user = True
+
+password = UserInformation.get_password(new_username,user_data)
+balance = UserInformation.get_balance(new_username, user_data)
+new_balance = 15000
+UserInformation.set_balance(new_username, balance, user_data, new_balance)
+print(balance)
 #user_name = input('Enter username: ')
 #verify_user(user_name, user_data)
--- user_information.py
+++ user_information.py
@@ -10,11 +10,26 @@ class UserInformation:
                 username, password, balance = row
                 data[username] = {'password': password, 'balance': float(balance)}
         return data
-
+    
+    
     def add_user_to_database(filename, new_username, new_password):
         with open(filename, 'a', newline = '\n') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow([new_username, new_password, 10000])
+    
+    def get_balance(username, user_data):
+        return user_data[username]['balance']
+    
+    def set_balance(username,password, user_data, amount):
+        user_data[username]['balance'] = amount
+        with open(filename, 'w', newline = '\n') as file:
+            csv_writer = csv.writer(file)
+            for user, data in user_data.items():
+                csv_writer.writerow([user, data['password'], data['balance']])
+                
+    def get_password(username, user_data):
+        return user_data[username]['balance']
+
 
 
 
@@ -31,3 +46,5 @@ class UserInformation:
             
         print('Invalid user')
 
+filename = 'casino_data.csv'
+user_data = UserInformation.read_csv_file(filename)
