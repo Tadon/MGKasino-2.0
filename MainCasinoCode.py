cfca381dcb092a2bda764fbd1d595d718fa517af
Tadon <Mark.Klocek@GMail.com>
2023-09-08 15:27:17 -0700

    Allowing already existing users to log in

--- MainCasinoCode.py
+++ MainCasinoCode.py
@@ -8,19 +8,32 @@ user_data = UserInformation.read_csv_file(filename)
 #Making user login/create user profile before allowing access to casino
 verified_user = False
 while verified_user == False:
-     new_username = input("Enter new user name or type exit to exit: ").lower()
-     if new_username.lower() == 'exit':
+     username = input("Enter username, or new user type desired username to create new account. Type exit to exit ").lower()
+     
+     if username.lower() == 'exit':
           verified_user = True
           break
      
-     if new_username in user_data:
-          print('Username already exists!')
+     if username in user_data:
+          password = input(f"Please enter password for {username}: ")
+          pw_check = UserInformation.get_password(username, user_data)
+          if password == pw_check:
+               balance = UserInformation.get_balance(username, user_data)
+               print(f'Welcome back, {username}! Your balance is {balance}.')
+               verified_user = True
+          else:
+               print('Invalid password. Please try again')
+               break
     
      else:
-          new_password = input('Enter password: ')
-          UserInformation.add_user_to_database(filename, new_username, new_password)
-          print(f'User added! Welcome to MGKasino 2.0, {new_username}!')
-          user_data[new_username] = {'password': new_password, 'balance': 10000}
+          password = input('Enter desired password: ')
+          pw_check = input('Please re-enter password: ')
+          while password != pw_check:
+               password = input('Passwords do not match. Re-enter password')
+               pw_check = input('Enter password again')
+          UserInformation.add_user_to_database(filename, username, password)
+          print(f'User added! Welcome to MGKasino 2.0, {username}!')
+          user_data[username] = {'password': password, 'balance': 10000}
           verified_user = True
 
 
