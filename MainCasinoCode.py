34e1f89f279729932ebd46a4ef49e80eee2dc5ed
Tadon <Mark.Klocek@GMail.com>
2023-09-09 16:43:01 -0700

    user info
    Cleaned up main .py and moved user function into user_info file and class.

--- MainCasinoCode.py
+++ MainCasinoCode.py
@@ -1,41 +1,18 @@
 #pulling user functions from user_information.py
 from user_information import UserInformation
 
-#creating filename variable to use read_csv file function to create hash of current users, passwords, and balances(user data)
+#required variables
 filename = 'casino_data.csv'
 user_data = UserInformation.read_csv_file(filename)
+username = ''
+password = ''
+balance = 0
 
-#Making user login/create user profile before allowing access to casino
-verified_user = False
-while verified_user == False:
-     username = input("Enter username, or new user type desired username to create new account. Type exit to exit ").lower()
-     
-     if username.lower() == 'exit':
-          verified_user = True
-          break
-     
-     if username in user_data:
-          password = input(f"Please enter password for {username}: ")
-          pw_check = UserInformation.get_password(username, user_data)
-          if password == pw_check:
-               balance = UserInformation.get_balance(username, user_data)
-               print(f'Welcome back, {username}! Your balance is {balance}.')
-               verified_user = True
-          else:
-               print('Invalid password. Please try again')
-               break
-    
-     else:
-          password = input('Enter desired password: ')
-          pw_check = input('Please re-enter password: ')
-          while password != pw_check:
-               password = input('Passwords do not match. Re-enter password: ')
-               pw_check = input('Enter password again: ')
-          UserInformation.add_user_to_database(filename, username, password)
-          print(f'User added! Welcome to MGKasino 2.0, {username}!')
-          user_data[username] = {'password': password, 'balance': 10000}
-          verified_user = True
+#Gathering username, balance, and password so we can game!
 
+while username == '':
+     username = UserInformation.user_verification(user_data)
+     balance = UserInformation.get_balance(username, user_data)
+     password = UserInformation.get_password(username, user_data)
 
-#user_name = input('Enter username: ')
-#verify_user(user_name, user_data)
+print(f'{username} with password {password} has a balance of ${balance}.')
--- user_information.py
+++ user_information.py
@@ -39,19 +39,44 @@ class UserInformation:
             csv_writer = csv.writer(file)
             for user, data in user_data.items():
                 csv_writer.writerow([user, data['password'], data['balance']])
-    #verify user method
-    def verify_user(user_name, user_data):
-        for user in user_data:
-            valid_user = False
-            if user['username'] == str(user_name):
-                valid_user = True
-                break
 
-        if valid_user == True:
-                print('Valid user!')
-                return
+    #method to get or create user from login
+    def user_verification(user_data):
+        not_verified = True
+        while not_verified == True:
+            username = input("Enter username, or new user type desired username to create new account. Type exit to exit ").lower()
             
-        print('Invalid user')
+            if username.lower() == '':
+                 print('Invalid username. Username cannot be empty')
+                 break
+            
+            if username.lower() == 'exit':
+                not_verified = False
+                break
+            
+            if username in user_data:
+                password = input(f"Please enter password for {username}: ")
+                pw_check = UserInformation.get_password(username, user_data)
+                if password == pw_check:
+                        balance = UserInformation.get_balance(username, user_data)
+                        print(f'Welcome back, {username}! Your balance is {balance}.')
+                        not_verified = False
+                        return username
+                        
+                else:
+                        print('Invalid password. Please try again')
+                                
+            else:
+                password = input('Enter desired password: ')
+                pw_check = input('Please re-enter password: ')
+                while password != pw_check:
+                        password = input('Passwords do not match. Re-enter password: ')
+                        pw_check = input('Enter password again: ')
+                UserInformation.add_user_to_database(filename, username, password)
+                print(f'User added! Welcome to MGKasino 2.0, {username}!')
+                user_data[username] = {'password': password, 'balance': 10000}
+                not_verified = False
+                return username
 
 filename = 'casino_data.csv'
 user_data = UserInformation.read_csv_file(filename)
