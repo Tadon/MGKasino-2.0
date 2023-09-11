1db46e15d11b8eb648006a132ea73d15bf821752
Tadon <Mark.Klocek@GMail.com>
2023-09-11 03:04:06 -0700

    bj logic update

--- blackjack_functionality.py
+++ blackjack_functionality.py
@@ -5,8 +5,9 @@ dealer_value = 0
 player_value = 0
 random_int = 0
 hand_in_progress = True
-while hand_in_progress == True:
-    dealer_hand = []
+for i in range(0, 10):
+    dealer_first_card = CardFunctions.get_card()
+    dealer_hand = [dealer_first_card]
     player_hand = []
     next_card = CardFunctions.get_card()
     print(f"{next_card['number']}{next_card['suit']}{next_card['value']}")
