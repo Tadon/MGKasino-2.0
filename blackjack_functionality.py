3b795b894828b8f43910a0d6f4e1a0ca9e434855
Tadon <Mark.Klocek@GMail.com>
2023-09-13 18:44:16 -0700

    n/a
    just adding user

--- blackjack_functionality.py
+++ blackjack_functionality.py
@@ -5,9 +5,10 @@ dealer_value = 0
 player_value = 0
 random_int = 0
 hand_in_progress = True
-for i in range(0, 10):
-    dealer_first_card = CardFunctions.get_card()
-    dealer_hand = [dealer_first_card]
-    player_hand = []
-    next_card = CardFunctions.get_card()
-    print(f"{next_card['number']}{next_card['suit']}{next_card['value']}")
+while hand_in_progress == True:
+    bj_deck = CardFunctions.deck_generator(CardFunctions.card_deck)
+    dealer_hand = []
+    dealer_hand.append(bj_deck.pop())
+    print(dealer_hand[0]['number']['suit'])
+    hand_in_progress = False
+    
