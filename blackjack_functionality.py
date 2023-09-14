5397ca227760a8c57b897772d110ac4a42b37d12
Tadon <Mark.Klocek@GMail.com>
2023-09-14 02:24:50 -0700

    Can deal a hand!
    Finally able to deal a hand in blackjack.

--- blackjack_functionality.py
+++ blackjack_functionality.py
@@ -5,10 +5,28 @@ dealer_value = 0
 player_value = 0
 random_int = 0
 hand_in_progress = True
-while hand_in_progress == True:
-    bj_deck = CardFunctions.deck_generator(CardFunctions.card_deck)
-    dealer_hand = []
+bj_deck = CardFunctions.deck_generator(CardFunctions.card_deck)
+dealer_hand = []
+player_hand = []
+while hand_in_progress:
+    player_info = ""
+    dealer_info = ""
     dealer_hand.append(bj_deck.pop())
-    print(dealer_hand[0]['number']['suit'])
+    player_hand.append(bj_deck.pop())
+    player_hand.append(bj_deck.pop())
+    for i in dealer_hand:
+        dealer_info += i['number'] + i['suit'] + ''
+        if i['number'] == '[A ' and i['value2'] + dealer_value < 22:
+            dealer_value += i['value2']
+        else:
+            dealer_value += i['value']
+    for i in player_hand:
+        player_info += i['number']+i['suit'] + ''
+        if i['number'] == '[A ' and i['value2'] + player_value < 22:
+            player_value += i['value2']
+        else:
+            player_value += i['value']
+
     hand_in_progress = False
-    
+print(f'Dealer\'s hand: {dealer_info} with a value of {dealer_value}')
+print(f'Player\'s hand: {player_info} with a value of {player_value}')
